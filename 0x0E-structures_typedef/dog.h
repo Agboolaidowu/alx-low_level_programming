7d2b2a6e8bad6616372902ee92001ea8951c0f2b
Agboola Idowu <techywid@gmail.com>
2022-08-01 12:22:18 -0700

    The header file for new type struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -17,6 +17,7 @@ struct dog
 	char *owner;
 };
 
+struct dog;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
@@ -24,5 +25,6 @@ dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 char *_strcpy(char *dest, char *src);
 int _strlen(char *s);
+
 
 #endif
