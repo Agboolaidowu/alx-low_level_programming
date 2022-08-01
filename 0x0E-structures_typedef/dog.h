50ab139aef206ea41f3a153eff917a144a5d08a6
Agboola Idowu <techywid@gmail.com>
2022-08-01 14:28:47 -0700

    header file updated

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -22,11 +22,7 @@ struct dog
  */
 
 typedef struct dog dog_t;
-{
-	char *name;
-	float age;
-	char *owner;
-}
+
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
